653d085c0473381b74bd69261e4ea848af16ab01
SBot23 <122625682+SBot23@users.noreply.github.com>
2023-01-21 21:04:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['918999112489'] // Put your number here
+global.owner = ['919526016012'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
